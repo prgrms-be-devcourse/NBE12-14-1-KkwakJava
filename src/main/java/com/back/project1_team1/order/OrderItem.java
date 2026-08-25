e29940c30e4f660492a9eecbdb49efa91350c953
sowon <kangsw2942@gmail.com>
2026-08-25 09:20:06 +0900

    [project1_team1]강소원 (#8)
    * 상품, 주문, 주문 상품 Entity 1차 작성 & 주문 레포지토리, 상품 레포지토리 기본 틀 작성
    
    * OrderItemRepository 파일 추가
    
    * Order패키지에 dto패키지 생성 & dto 패키지에 주문생성요청dto, 주문상품dto 클래스 생성
    
    * product등록 dto 생성

--- src/main/java/com/back/project1_team1/order/OrderItem.java
+++ src/main/java/com/back/project1_team1/order/OrderItem.java
@@ -1,4 +1,23 @@
 package com.back.project1_team1.order;
 
-public class OrderItem {
+import com.back.project1_team1.product.Product;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem{
+
+    @Id // 주문 상품 테이블의 기본키
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 +1
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id") // 주문 테이블과 join
+    private Order order; // 주문 테이블 객체
+
+    @ManyToOne
+    @JoinColumn(name = "product_id") // 상품 테이블과 join
+    private Product product; // 상품 테이블 객체
+
+    private int quantity; // 해당 주문 상품 수량
 }
---
+++ src/main/java/com/back/project1_team1/order/dto/OrderCreateRequest.java
@@ -0,0 +1,17 @@
+package com.back.project1_team1.order.dto;
+
+import java.util.List;
+
+/*
+    주문 생성 API(POST /orders)의 요청 데이터를 받는 DTO
+    고객 이메일과 주문할 상품 목록을 클라이언트로부터 전달받음
+    주문 ID는 DB에서 자동 생성, 주문 시각은 서버에서 생성하므로
+    클라이언트에서는 고객 이메일, 상룸 목록만 전달
+ */
+
+public class OrderCreateRequest {
+
+    private String email; // 고객 이메일
+
+    private List<OrderItemRequest> items; // 한 건의 주문에 포함된 상품 목록
+}
