dc61979972e0ebbed4e25b65d82f1855678b8d11
seul <l22seul43@gmail.com>
2026-08-25 11:27:13 +0900

    delete 기능 구현(1)

--- src/main/java/com/back/project1_team1/order/OrderController.java
+++ src/main/java/com/back/project1_team1/order/OrderController.java
@@ -1,4 +1,37 @@
 package com.back.project1_team1.order;
 
+import org.osgi.annotation.versioning.ProviderType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/orders")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @DeleteMapping("/{orderId}")
+   public void deleteOrder(
+       @PathVariable Integer orderId,
+        Model model
+    ){
+        try{
+            orderService.deleteOrder(orderId);
+        }catch (IllegalArgumentException e){
+            model.addAttribute("error", "존재하지 않는 주문입니다");
+        }catch (IllegalStateException e){
+            model.addAttribute("error", e.getMessage());
+        }
+            return "redirect:/orders"; //삭제 후 목록으로 이동
+
+    }
+
 }
--- src/main/java/com/back/project1_team1/order/OrderService.java
+++ src/main/java/com/back/project1_team1/order/OrderService.java
@@ -1,4 +1,23 @@
 package com.back.project1_team1.order;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public void deleteOrder(int orderId){
+        Order order = orderRepository.findById(orderId)
+            .orElseThrow (() -> new IllegalArgumentException("존재하지 않는 주문입니다,. id = " +orderId));
+
+        orderRepository.delete(order);
+    }
 }
