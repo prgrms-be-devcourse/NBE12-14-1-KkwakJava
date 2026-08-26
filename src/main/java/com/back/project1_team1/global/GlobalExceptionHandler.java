9583bb4ee257730eca2034eae0cf3763a990bdf0
jkidse14 <jkidse14@gamil.com>
2026-08-27 00:29:01 +0900

    fix: validation 예외 메시지 다건 반환

--- src/main/java/com/back/project1_team1/global/GlobalExceptionHandler.java
+++ src/main/java/com/back/project1_team1/global/GlobalExceptionHandler.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // 에러 응답용 표준 Record DTO
-    public record ErrorResponse(int status, String error, String message) {
+    public record ErrorResponse(int status, String error, Object message) {
     }
 
     // 400 BAD_REQUEST: DTO Validation(@Valid) 검증 실패
@@ -19,18 +21,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(
         MethodArgumentNotValidException e) {
 
-        FieldError fieldError = e.getBindingResult().getFieldError();
-
-        String message = (fieldError != null)
-            ? fieldError.getDefaultMessage()
-            : "유효하지 않은 요청입니다.";
+        List<String> messages = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
 
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(new ErrorResponse(
                 400,
                 "BAD_REQUEST",
-                message
+                messages
             ));
     }
 
