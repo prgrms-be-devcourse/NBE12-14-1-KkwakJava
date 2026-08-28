c308e811adb9a9c0e0e3f455109fd4d003488e53
GibGui <hsy17026@gmail.com>
2026-08-28 17:14:36 +0900

    feat: 기본 경로 리다이렉트 규칙 추가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination:"/order",
+        permanent: false,
+      },
+      {
+        source: "/admin",
+        destination: "/admin/delivery",
+        permanent: false
+      },
+    ];
+  },
 };
 
 export default nextConfig;
