1da508a50bc8f8d6819d9a7da850614dba3eb51c
GibGui <hsy17026@gmail.com>
2026-08-28 10:33:44 +0900

    fix: 불필요한 import문 삭제

--- src/main/java/com/back/project1_team1/order/dto/OrderUpdateRequest.java
+++ src/main/java/com/back/project1_team1/order/dto/OrderUpdateRequest.java
@@ -1,7 +1,6 @@
 package com.back.project1_team1.order.dto;
 
 
-import com.back.project1_team1.order.OrderItemRepository;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
